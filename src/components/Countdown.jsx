a310a3a05248bc1890bd9378dec4ed3cc151fec0
deborararumairaj <deborararumairaj@gmail.com>
2025-12-27 15:22:09 +0100

    Update Countdown.jsx

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -76,7 +76,7 @@ function Countdown({ onBirthdayReached, birthdayReached }) {
       <section className="countdown">
         <div className="flip-timer">
           <span className="birthday-celebration">
-            🎉 It's Your Birthday! 🎉
+            🎉 It's my special girl's Birthday! 🎉
           </span>
         </div>
       </section>
